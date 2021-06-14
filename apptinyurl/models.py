d615a919837b21fd3791af7ac4b3ccbdd792ce6c
Dmitry <loldragonmovies@gmail.com>
2021-06-14 07:28:57 +0300

    urls model has been added to db

--- apptinyurl/models.py
+++ apptinyurl/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Urls(models.Model):
+    short_url = models.CharField(max_length=8, primary_key=True)
+    long_url = models.URLField(max_length=100)
+    clicks_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.long_url
